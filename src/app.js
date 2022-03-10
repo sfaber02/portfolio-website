3c10dd9d49b167e36a02a682fde2980528e6da52
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-03-09 21:29:30 -0500

    added basic nav bar

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import React from 'react';
+import { TopBar } from './topbar';
+
+const App = () => {
+
+    return (
+        <div id="mainContainer">
+            <TopBar />
+        </div>
+    );
+}
+
+export { App };
---
+++ src/topbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TopBar = () => {
+
+    return (
+        <div id="topBar">
+            <h2 className='topBarButtons'>About</h2>
+            <h2 className='topBarButtons'>Something</h2>
+            <h2 className='topBarButtons'>Top Bar 3</h2>
+            <h2 className='topBarButtons'>Top Bar 4</h2>
+            <h2 className='topBarButtons'>Top Bar 5</h2>
+        </div>
+    );
+}
+
+export { TopBar };
