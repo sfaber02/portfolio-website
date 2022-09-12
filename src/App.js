40c8f8c04f17d6fa6c5d489f0017c3982e5f69d8
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-09-11 22:34:59 -0400

    commit before merge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 // import "./Styles/style.css";
-import React, { useState} from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
 
 /**
  * The various pages of my resume
  */
 import { LandingPage } from "./Components/LandingPage";
-import { Contact } from './Components/Contact.js';
+import { Contact } from "./Components/Contact.js";
 import { Experience } from "./Components/Experience.js";
 import { Projects } from "./Components/Projects.js";
-import { Skills } from "./Components/Skills.js"; 
-
+import { Skills } from "./Components/Skills.js";
 
 const App = () => {
     const [firstLoad, setFirstLoad] = useState(true);
@@ -21,14 +19,22 @@ const App = () => {
     return (
         <div className="App">
             <Routes>
-                <Route path='/' element={<LandingPage firstLoad={firstLoad} toggleFirstLoad={toggleFirstLoad} />} />
-                <Route path='/projects' element={<Projects />} />
-                <Route path='/skills' element={<Skills />} />
-                <Route path='/experience' element={<Experience />} />
-                <Route path='/contact' element={<Contact />} />
+                <Route
+                    path="/"
+                    element={
+                        <LandingPage
+                            firstLoad={firstLoad}
+                            toggleFirstLoad={toggleFirstLoad}
+                        />
+                    }
+                />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/experience" element={<Experience />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </div>
     );
-}
+};
 
 export { App };
